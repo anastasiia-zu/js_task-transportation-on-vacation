33715d93a99d1d0c526a54f9c9066684cd3709fd
Anastasiia <anastasiia.zuieva4@gmail.com>
2025-02-10 19:49:48 +0200

    first

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,19 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const total = 40 * days;
+
+  if (days >= 7) {
+    return total - 50;
+  }
+
+  if (days >= 3) {
+    return total - 20;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
